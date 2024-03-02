98a3ba276df3d82fd4936fc0511710e9e6a51224
Md Shahid Alam <md.shahidalam.cse24@heritageit.edu.in>
2024-03-02 12:15:41 +0530

    Figlet for run first npm install to download dependency or node modules

---
+++ 07.Figlet/index.js
@@ -0,0 +1,10 @@
+let figlet = require("figlet");
+
+figlet("Hello MS 2.O", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
